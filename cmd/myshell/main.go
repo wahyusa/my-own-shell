b887455d115ef4758bd095a15002bb21ee1f1d6a
wahyusa <wahyuskull22@gmail.com>
2024-05-25 20:31:11 +0800

    try to loop

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,21 +8,25 @@ import (
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	reader := bufio.NewReader(os.Stdin)
 
-	// Wait for user input
-	// bufio.NewReader(os.Stdin).ReadString('\n')
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// check if user input command is not reognized, then print error message and wait for user input again
+		commandInput, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
 
-	commandInput, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println(err.Error())
+		commandInput = strings.TrimSpace(commandInput)
+
+		if commandInput == "exit" {
+			break
+		}
+
+		// Here you can add code to evaluate the commandInput
+		// For now, we just print "command not found"
+		fmt.Printf("%s: command not found\n", commandInput)
 	}
-
-	commandInput = strings.TrimRight(commandInput, "\n")
-
-	// Print "command not found"
-	fmt.Printf("%s: command not found\n", commandInput)
 }
