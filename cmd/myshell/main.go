2eec4bc82c99dddccdd5428fb46b6bbb4d7a2d5f
wahyusa <wahyuskull22@gmail.com>
2024-05-25 20:24:36 +0800

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,15 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	// fmt.Println("Logs from your program will appear here!")
-
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	// bufio.NewReader(os.Stdin).ReadString('\n')
+
+	// check if user input command is not reognized, then print error message and wait for user input again
+
+	commandInput, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	commandInput = strings.TrimRight(commandInput, "\n")
+
+	// Print "command not found"
+	fmt.Printf("%s: command not found\n", commandInput)
 }
